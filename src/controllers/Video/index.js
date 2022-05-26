1031ab126d630a8b74f46a2ab73acd3a20b87d02
Roman <efim1382@gmail.com>
2022-05-26 19:56:43 +0300

    git status

--- src/controllers/Video/index.js
+++ src/controllers/Video/index.js
@@ -1,31 +1,35 @@
 import fs from "fs";
+import VideoModel from "models/Video";
 import { formatErrorResponse } from "helpers/formatResponse";
 
 const Video = async (req, res) => {
-  const range = req.headers.range;
-  const videoPath = `${__dirname}/test.mp4`;
-  const videoSize = fs.statSync(videoPath).size;
-
-  const chunkSize = 1e+6;
-  const start = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + chunkSize, videoSize - 1);
-
-  const contentLength = end - start + 1;
-
-  const headers = {
-    "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-    "Accept-Ranges": "bytes",
-    "Content-Length": contentLength,
-    "Content-Type": "video/mp4",
-  };
+  const { range } = req.headers;
+  const { id } = req.params;
 
   try {
+    const video = await VideoModel.findOne({ _id: id });
+    const videoPath = `${__dirname}/${video.path}`;
+    const videoSize = fs.statSync(videoPath).size;
+
+    const chunkSize = 1e+6;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + chunkSize, videoSize - 1);
+
+    const contentLength = end - start + 1;
+
+    const headers = {
+      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+      "Accept-Ranges": "bytes",
+      "Content-Length": contentLength,
+      "Content-Type": "video/mp4",
+    };
+
     res.writeHead(206, headers);
     const stream = fs.createReadStream(videoPath, { start, end });
     stream.pipe(res);
   } catch (error) {
     console.log(error);
-    res.json(formatErrorResponse(error));
+    res.json(formatErrorResponse(error)); // refactore with sending status
   }
 };
 
